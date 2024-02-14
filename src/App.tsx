09f1ee7eb4f12955bc2a20fa3aeb649fd2f522ad
Francesco Marino <francescofriy@gmail.com>
2024-02-14 17:43:13 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React, { Dispatch } from "react";
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const {
-    counter,
-    setCounter,
-  }: { counter: number; setCounter: Dispatch<SetStateAction<number>> } =
-    useState(0);
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <button onClick={setCounter(counter - 1)}>--</button>
-      <h1 style={{ color = counter > 0 ? "green" : "red" }}>{counter}</h1>
-      <button onClick={setCounter(counter + 1)}>++</button>
+      <button onClick={() => setCounter(counter - 1)}>--</button>
+      <h1 style={{ color: counter > -1 ? "green" : "red" }}>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>++</button>
     </div>
   );
 }
