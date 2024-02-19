cf17e9500e28ff3bbbf0c40b78a4cbea56e40c78
Annalisa Nasca <annalisanasca06@gmail.com>
2024-02-19 12:16:25 +0100

    Ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-=======
->>>>>>> 38ee2a768fa8dbc5e06f2fcc9cff6e2a86ff52ab
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
